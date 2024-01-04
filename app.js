c60edd130ad1b804748c157cdbbc6da6791a1092
muhammad <muhammadhaziqhabibkhan76@gmail.com>
2024-01-04 18:57:52 +0500

    comparision product

---
+++ app.js
@@ -0,0 +1,11 @@
+for(let i=1;i<products.length;i++){
+    document.getElementById('select1').innerHTML +=`
+    <option value="${i}">${products[i].title}</option>
+    `
+}
+
+for(let i=1;i<products.length;i++){
+    document.getElementById('select2').innerHTML +=`
+    <option value="${i}">${products[i].title}</option>
+    `
+}
