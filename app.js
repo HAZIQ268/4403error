1fa3316ba1dd485721d9c63d9aca3bfe1b668c3d
muhammad <muhammadhaziqhabibkhan76@gmail.com>
2024-01-06 18:13:38 +0500

    comparision product

--- app.js
+++ app.js
@@ -9,3 +9,17 @@ for(let i=1;i<products.length;i++){
     <option value="${i}">${products[i].title}</option>
     `
 }
+
+function item1(a){
+    document.getElementById('img1').src =products[a].image;
+    document.getElementById('price1').innerHTML = products[a].price
+    document.getElementById('brand1').innerHTML = products[a].brand
+    document.getElementById('desc1').innerHTML = products[a].description
+}
+
+function item2(a){
+    document.getElementById('img2').src = products[a].image;
+    document.getElementById('price2').innerHTML = products[a].price
+    document.getElementById('brand2').innerHTML = products[a].brand
+    document.getElementById('desc2').innerHTML = products[a].description
+}
